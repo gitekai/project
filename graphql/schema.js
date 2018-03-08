22e33aa841aa1aba05190dec796d2dd772a679ad
romeo.hesch <romeo.hesch@gmail.com>
2018-03-08 15:21:56 +0100

    Query mequanism functions more or less

--- graphql/schema.js
+++ graphql/schema.js
@@ -6,109 +6,60 @@ const RazonSocial = require('./schemaParts/razonSocial');
 const GrupoEmpresarial = require('./schemaParts/grupoEmpresarial');
 const Op = require('sequelize').Op;
 
-/*
-function createWhere(root, res = {}) {
-  Object.keys(root).forEach((key) => {
 
-    let operator;
-    let field;
-    if (/(OR|AND)/.test(key)) {
-      operator = key;
-    } else {
-      [field, operator] = key.split('_')
-    }
-    const operatorObj = {};
+function modifyWhere(obj, res = {}) {
+  if (!obj.AND && !obj.OR) {
+    Object.keys(obj).forEach((key) => {
 
-    if (operator == "equals") {
-      operatorObj[field] = { [Op.eq]: root[key] };
-      res = operatorObj;
-      root = (typeof (root[key]) == "object") ? root[key] : {};
-      createWhere(root, res);
-    }
-    else if (operator == "like") {
-      operatorObj[field] = { [Op.like]: root[key] };
-      res = operatorObj;
-      root = (typeof (root[key]) == "object") ? root[key] : {};
-      createWhere(root, res);
-    } else if (operator == "OR") {
-      //Array o object
-      if (Array.isArray(root[key])) { // its always because of graphql 
-        root[key].forEach((item) => {
-          if (Array.isArray(res[Op.or])) {
-            res[Op.or].push(item);
-          } else {
-            // initializo el objeto como array
-            res[Op.or] = [item];
-          }
-        });
+      let [attr, operator] = key.split('_');
+
+      let a = {};
+      a[attr] = {};
+      if (operator == 'like') {
+        a[attr][Op.like] = obj[key];
+      } else if (operator == 'equals') {
+        a[attr][Op.eq] = obj[key];
       }
 
-
-
-      //estripar y meter en operador
-      //createWhere()
-      console.log("");
-    } else if (operator == "AND") {
-      //estripar y meter en operador
-      // no puede haber un AND vacio está controlado por graphql 
-      //createWhere()
-      console.log("");
-    }
-
-  });
-
-  if (objIsEmpty(root)) {
-    // hemos llegado a la condicion final 
-    return res;
-  }
-}
-
-*/
-function createWhere(root, res = {}, pointer) {
-
-
-  if (Array.isArray(root)) {
-    //AND OR OR operators
-    root.forEach((item) => {
-      if (typeof item == 'object') {
-        createWhere(item, res);
-      }
+      obj[attr] = {};
+      Object.defineProperties(obj,
+        {
+          [attr]: Object.getOwnPropertyDescriptor(a, attr),
+        }
+      );
+      delete obj[key];
     });
-  } else {
-    //convertir obj in sequelizable 
-    const operatorObj = {};
+    return obj;
+  }
 
-    Object.keys(root).forEach((key) => {
-      let operator;
-      let field;
-      if (/(OR|AND)/.test(key)) {
-        operator = key;
-      } else {
-        [field, operator] = key.split('_')
-      }
-      if (operator == "equals") {
-        operatorObj[field] = { [Op.eq]: root[key] };
-      } else if (operator == "like") {
-        operatorObj[field] = { [Op.like]: root[key] };
-      }
-
-      if (operator == "OR") {
-        createWhere(root[key], res[Op.or]=[]);
-      } else if (operator == "AND") {
-        createWhere(root[key], res[Op.and]=[]);
-      }
-
-      if (Array.isArray(res)) {
-        res.push(operatorObj);
-      } else {
-        // initializo el objeto como array
-        res = operatorObj;
-      }
+  if (obj.AND) {
+    Object.defineProperties(obj,
+      {
+        [Op.and]: Object.getOwnPropertyDescriptor(obj, 'AND'),
+      });
+    delete obj.AND;
+    obj[Op.and].forEach((item) => {
+      modifyWhere(item);
     });
   }
-  return res;
+
+
+
+  if (obj.OR) {
+    Object.defineProperties(obj,
+      {
+        [Op.or]: Object.getOwnPropertyDescriptor(obj, 'OR'),
+      });
+
+    delete obj.OR;
+    obj[Op.or].forEach((item) => {
+      modifyWhere(item);
+    });
+  }
+
 
 }
+
 
 
 //filter[Op.and].push(operatorObj);
@@ -118,25 +69,6 @@ function objIsEmpty(obj) {
     && true
     || false;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const RootQuery = `
@@ -161,50 +93,14 @@ const SchemaDefinition = `
 const resolvers = {
   RootQuery: {
     razonesSociales: (obj, args, context, info) => {
-
-
-      /*
-      let filter = {
-        [Op.and]: [],
-        //[Op.or]: [],      
-      };
-      Object.keys(where).forEach((key) => {
-        const [field, operator] = key.split('_');
-
-        const operatorObj = {};
-        operatorObj[field] = { [Op.like]: where[key] };
-
-        if (operator == "equals") {
-          operatorObj[field] = { [Op.eq]: where[key] };
-        }
-        else if (operator == "like") {
-          operatorObj[field] = { [Op.like]: where[key] };
-        } else if (operator == "OR") {
-          //strip that shit
-        }
-
-
-
-        filter[Op.and].push(operatorObj);
-        /*filter = {
-          [Op.and]: [
-            operatorObj,
-          ],
-        };
-
-
-      });*/
       const { where = {}, first = 10, skip = 0 } = args;
-      let o = createWhere(where);
-      console.log("where");
+      modifyWhere(where);
       return models.RazonesSociales.findAll({
         limit: args.first,
         offset: args.skip,
-        where: o,
-
+        where: where,
       });
     },
-    //dels.RazonesSociales.findAll({ where:{ [op.and]: { nombre: 'kk' , url: 'pipi'}} })
   }
 };
 
